f3725825fc75f436eab8b90c6e8d77d9724d2fb6
konmenel <konmenel@gmail.com>
2023-03-08 17:05:09 +0000

    Package restructure, waves module created

--- pydsphtools/__init__.py
+++ pydsphtools/__init__.py
@@ -17,4 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 @github: https://github.com/konmenel
 @year: 2023
 """
+from . import waves, exceptions
 from .main import *
+
+__all__ = ["waves", "exceptions"]
