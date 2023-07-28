7cd62fd637df6124a8ce30c1ec06bba8ffbd4d53
konmenel <konmenel@gmail.com>
2023-07-28 16:59:19 +0100

    Fix doc mistakes

--- pydsphtools/waves.py
+++ pydsphtools/waves.py
@@ -1,8 +1,8 @@
 """Module containing functions useful when working with oceaning waves.
 
-This file is part of Foo Project. It is subject to the license terms in the
+This file is part of PyDSPHtools. It is subject to the license terms in the
 LICENSE file found in the top-level directory of this distribution and at
-https://github.com/konmenel/pydsphtools/blob/main/LICENSE. No part of Foo Project,
+https://github.com/konmenel/pydsphtools/blob/main/LICENSE. No part of PyDSPHtools,
 including this file, may be copied, modified, propagated, or distributed except
 according to the terms contained in the LICENSE file.
 
