c3b886e6b973cefe17c93289503befe07d8d1462
konmenel <konmenel@gmail.com>
2024-07-23 13:36:57 +0100

    Impelented reading Items expect arrays

--- pydsphtools/_io.py
+++ pydsphtools/_io.py
@@ -111,9 +111,9 @@ class Endianness(Enum):
     little = 0
     big = 1
 
-    @staticmethod
-    def from_bytes(bytes: bytes) -> Endianness:
-        return Endianness(int.from_bytes(bytes))
+    @classmethod
+    def from_bytes(cls, bytes: bytes) -> Endianness:
+        return cls(int.from_bytes(bytes))
 
 
 class Array:
@@ -124,8 +124,12 @@ class Array:
     array_size: int
     data: list[None | bool | str | int | float | tuple[float, float, float]]
 
+    def __init__(self) -> None:
+        raise NotImplementedError
+
 
 class Item:
+    item_size: int
     name: str
     hide: bool
     hide_values: bool
@@ -134,12 +138,28 @@ class Item:
     num_arrays: int
     num_items: int
     size_values: int
-    values: list[None | bool | str | int | float | tuple[float, float, float]]
+    values: dict[str, None | bool | str | int | float | tuple[float, float, float]]
     items: list[Item]
-    arrays: list[Array]
+    # Arrays should not be loaded as they can be too big
+    # arrays: list[Array]
+
+    __slots__ = (
+        "_item_size",
+        "_name",
+        "_hide",
+        "_hide_values",
+        "_fmt_float",
+        "_fmt_double",
+        "_num_arrays",
+        "_num_items",
+        "_size_values",
+        "_values",
+        "_items",
+    )
 
     def __init__(
         self,
+        item_size: int,
         name: str,
         hide: bool,
         hide_values: bool,
@@ -149,9 +169,10 @@ class Item:
         num_items: int,
         size_values: int,
         values: dict[str, None | bool | str | int | float | tuple[float, float, float]],
-        # items: list[Item],
+        items: list[Item],
         # arrays: list[Array],
     ) -> None:
+        self._item_size = item_size
         self._name = name
         self._hide = hide
         self._hide_values = hide_values
@@ -161,11 +182,16 @@ class Item:
         self._num_items = num_items
         self._size_values = size_values
         self._values = values
-        # self._items = items
+        self._items = items
         # self._arrays = arrays
 
     @classmethod
-    def from_stream(cls: Item, stream: io.BytesIO, endianness: Endianness) -> Item:
+    def from_stream(
+        cls: Item, bytes_stream: io.BytesIO, endianness: Endianness
+    ) -> Item:
+        item_size = int.from_bytes(bytes_stream.read(4), endianness.name)
+        buff = bytes_stream.read(item_size)
+        stream = io.BytesIO(buff)
         item_str_size = int.from_bytes(stream.read(UINT_SIZE), endianness.name)
         assert item_str_size == 6, f"Expected 6 but found {item_str_size}"
         item_str = stream.read(item_str_size).decode("utf-8")
@@ -181,9 +207,13 @@ class Item:
         num_arrays = int.from_bytes(stream.read(UINT_SIZE), endianness.name)
         num_items = int.from_bytes(stream.read(UINT_SIZE), endianness.name)
         size_values = int.from_bytes(stream.read(UINT_SIZE), endianness.name)
+        assert stream.read() == b"", "Item buffer is not empty."
 
+        # Update buffer since we the items definition is finished
+        buff = bytes_stream.read(size_values)
+        stream = io.BytesIO(buff)
         values_str_size = int.from_bytes(stream.read(UINT_SIZE), endianness.name)
-        assert values_str_size == 7
+        assert values_str_size == 7, f"Expected 7 but found {values_str_size}"
         values_str = stream.read(values_str_size).decode("utf-8")
         assert (
             values_str == "\nVALUES"
@@ -194,7 +224,15 @@ class Item:
             for _ in range(num_values)
             for name, value in [Item._parse_value_from_stream(stream, endianness)]
         }
+        assert stream.read() == b"", "Values buffer is not empty."
+
+        # Update buffer since we reached the end of values
+        buff = bytes_stream.read(size_values)
+        stream = io.BytesIO(buff)
+        items = [Item.from_stream(stream, endianness) for _ in range(num_items)]
+
         return cls(
+            item_size,
             name,
             hide,
             hide_values,
@@ -204,19 +242,87 @@ class Item:
             num_items,
             size_values,
             values,
+            items,
         )
+
+    @classmethod
+    def from_bytes(cls, bytes: bytes, endianness: Endianness) -> Item:
+        return cls.from_stream(io.BytesIO(bytes), endianness)
+    
+    @property 
+    def item_size(self) -> int:
+        return self._item_size
 
     @property
     def name(self) -> str:
         return self._name
 
+    @property
+    def hide(self) -> bool:
+        return self._hide
+    
+    @property
+    def hide_values(self) -> bool:
+        return self._hide_values
+    
+    @property
+    def fmt_float(self) -> str:
+        return self._fmt_float
+
+    @property
+    def fmt_double(self) -> str:
+        return self._fmt_double
+    
+    @property
+    def num_arrays(self) -> int:
+        return self.num_arrays
+
+    @property
+    def num_items(self) -> int:
+        return self._num_items
+
+    @property
+    def size_values(self) -> int:
+        return self._size_values
+
+    @property
+    def values(self) -> dict[str, None | bool | str | int | float | tuple[float, float, float]]:
+        return self._values
+
+    @property
+    def items(self) -> list[Item]:
+        return self._items
+
+    def __str__(self) -> str:
+        return self.pretty_print()
+    
     def __repr__(self) -> str:
-        return (
-            f"Item(name={self._name},hide={self._hide},hide_values={self._hide_values},"
-            f"fmt_float={self._fmt_float},fmt_double={self._fmt_double},"
-            f"num_arrays={self._num_arrays},num_items={self._num_items},"
-            f"size_values={self._size_values},values={self._values})"
+        return self.pretty_print()
+
+    def pretty_print(self, indent=0) -> str:
+        indent_str = "  "
+        ret = (
+            f"{indent_str*indent}Item(\n"
+            f"{indent_str*(indent+1)}item_size={self._item_size},\n"
+            f"{indent_str*(indent+1)}name={self._name},\n"
+            f"{indent_str*(indent+1)}hide={self._hide},\n"
+            f"{indent_str*(indent+1)}hide_values={self._hide_values},\n"
+            f"{indent_str*(indent+1)}fmt_float={self._fmt_float},\n"
+            f"{indent_str*(indent+1)}fmt_double={self._fmt_double},\n"
+            f"{indent_str*(indent+1)}num_arrays={self._num_arrays},\n"
+            f"{indent_str*(indent+1)}num_items={self._num_items},\n"
+            f"{indent_str*(indent+1)}size_values={self._size_values},\n"
+            f"{indent_str*(indent+1)}values={self._values},\n"
         )
+        if self._num_items:
+            ret += f"{indent_str*(indent+1)}items=[\n"
+            for item in self._items:
+                ret += f"{indent_str*indent}{item.pretty_print(indent+2)}"
+            ret += f"{indent_str*(indent+1)}]\n"
+        else:
+            ret += f"{indent_str*(indent+1)}items=[]\n"
+        ret += f"{indent_str*indent})\n"
+        return ret
 
     @staticmethod
     def _parse_value_from_stream(
@@ -226,12 +332,11 @@ class Item:
         name_size = int.from_bytes(stream.read(UINT_SIZE), endianness.name)
         name = stream.read(name_size).decode("utf-8")
         data_type = DataType(int.from_bytes(stream.read(INT_SIZE), endianness.name))
+        edn_fmt = "<" if endianness == Endianness.little else ">"
 
         match data_type:
             case DataType.null:
                 return name, None
-            case DataType.bool:
-                return name, bool.from_bytes(stream.read(UINT_SIZE), endianness.name)
             case DataType.char | DataType.uchar:
                 return name, stream.read(CHAR_SIZE).decode("utf-8")
             case DataType.text:
@@ -239,14 +344,14 @@ class Item:
                 return name, stream.read(str_size).decode("utf-8")
             case DataType.short | DataType.ushort:
                 return name, int.from_bytes(stream.read(SHORT_SIZE), endianness.name)
-            case DataType.int | DataType.uint:
+            case DataType.int | DataType.uint | DataType.bool:
                 return name, int.from_bytes(stream.read(INT_SIZE), endianness.name)
             case DataType.llong | DataType.ullong:
                 return name, int.from_bytes(stream.read(LONG_SIZE), endianness.name)
             case DataType.float:
-                return name, struct.unpack("f", stream.read(FLOAT_SIZE))
+                return name, struct.unpack(f"{edn_fmt}f", stream.read(FLOAT_SIZE))[0]
             case DataType.double:
-                return name, struct.unpack("d", stream.read(DOUBLE_SIZE))
+                return name, struct.unpack(f"{edn_fmt}d", stream.read(DOUBLE_SIZE))[0]
             case DataType.int3 | DataType.uint3:
                 return name, (
                     int.from_bytes(stream.read(INT_SIZE), endianness.name),
@@ -254,22 +359,22 @@ class Item:
                     int.from_bytes(stream.read(INT_SIZE), endianness.name),
                 )
             case DataType.float3:
-                return name, (
-                    struct.unpack("f", stream.read(FLOAT_SIZE)),
-                    struct.unpack("f", stream.read(FLOAT_SIZE)),
-                    struct.unpack("f", stream.read(FLOAT_SIZE)),
-                )
+                return name, struct.unpack(f"{edn_fmt}3f", stream.read(FLOAT3_SIZE))
             case DataType.double3:
-                return name, (
-                    struct.unpack("d", stream.read(DOUBLE_SIZE)),
-                    struct.unpack("d", stream.read(DOUBLE_SIZE)),
-                    struct.unpack("d", stream.read(DOUBLE_SIZE)),
-                )
+                return name, struct.unpack(f"{edn_fmt}3d", stream.read(DOUBLE3_SIZE))
             case _:
                 raise NotImplementedError(
                     f"Can't parse value of data type {data_type}"
                     " because it is not implemented"
                 )
+
+
+class Bi4File:
+    filename: str
+    item: Item
+
+    def __init__(self, filepath: str | os.PathLike) -> None:
+        raise NotImplementedError
 
 
 def read_bi4(partfile: str | os.PathLike) -> None:
@@ -281,9 +386,9 @@ def read_bi4(partfile: str | os.PathLike) -> None:
         extra = file.read(3)
         print(f"{title}", end="")
         print(f"{extra}")
-        size = int.from_bytes(file.read(4), byteorder.name)
-        print(size)
-        print(Item.from_stream(file, byteorder))
+        main_item = Item.from_stream(file, byteorder)
+        print(main_item)
+
 
 
 if __name__ == "__main__":
